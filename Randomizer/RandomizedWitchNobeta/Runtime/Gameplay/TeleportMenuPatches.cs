8d3b15375deb1e1b529cd6041e141212077441ad
Eveldee <eveldee0680@live.fr>
2023-08-25 19:59:18 +0200

    Improve controller support in Teleport UI

--- Randomizer/RandomizedWitchNobeta/Runtime/Gameplay/TeleportMenuPatches.cs
+++ Randomizer/RandomizedWitchNobeta/Runtime/Gameplay/TeleportMenuPatches.cs
@@ -8,6 +8,10 @@ namespace RandomizedWitchNobeta.Runtime.Gameplay;
 public static class TeleportMenuPatches
 {
     private static GameObject[] _topHandlers;
+    private static UILabelHandler _returnHandler;
+    private static UILabelHandler _exitHandler;
+    private static UITeleportHandler _firstStage6Handler;
+    private static UITeleportHandler _firstStage1Handler;
 
     [HarmonyPatch(typeof(UITeleport), nameof(UITeleport.Init))]
     [HarmonyPostfix]
@@ -32,6 +36,39 @@ public static class TeleportMenuPatches
         // Store top handlers for later use
         _topHandlers = Enumerable.Range(0, handlers.childCount).Select(index => handlers.GetChild(index).gameObject).ToArray();
 
+        // Change how handlers are navigated to allow better controller support
+        var uiHandlers = handlers.GetComponentsInChildren<UITeleportHandler>(true);
+
+        var stage1Handlers = uiHandlers.Where(uiHandler => uiHandler.name.StartsWith("Handler_1")).ToArray();
+        var stage2Handlers = uiHandlers.Where(uiHandler => uiHandler.name.StartsWith("Handler_2")).ToArray();
+        var stage3Handlers = uiHandlers.Where(uiHandler => uiHandler.name.StartsWith("Handler_3")).ToArray();
+        var stage4Handlers = uiHandlers.Where(uiHandler => uiHandler.name.StartsWith("Handler_4")).ToArray();
+        var stage5Handlers = uiHandlers.Where(uiHandler => uiHandler.name.StartsWith("Handler_5")).ToArray();
+        var stage6Handlers = uiHandlers.Where(uiHandler => uiHandler.name.StartsWith("Handler_6")).ToArray();
+
+        // Vertical navigation
+        for (int i = 0; i < stage4Handlers.Length; i++)
+        {
+            stage4Handlers[i].selectDown = stage5Handlers[i];
+            stage5Handlers[i].selectUp = stage4Handlers[i];
+        }
+
+        // Horizontal navigation
+        stage1Handlers[0].selectLeft = stage5Handlers[^1];
+        stage2Handlers[0].selectLeft = stage6Handlers[^1];
+        stage1Handlers[^1].selectRight = stage5Handlers[0];
+        stage2Handlers[^1].selectRight = stage6Handlers[0];
+
+        stage5Handlers[0].selectLeft = stage1Handlers[^1];
+        stage6Handlers[0].selectLeft = stage2Handlers[^1];
+        stage5Handlers[^1].selectRight = stage1Handlers[0];
+        stage6Handlers[^1].selectRight = stage2Handlers[0];
+
+        _returnHandler = root.Find("ButtonHandlers/Back").GetComponent<UILabelHandler>();
+        _exitHandler = root.Find("ButtonHandlers/Close").GetComponent<UILabelHandler>();
+        _firstStage1Handler = stage1Handlers[0];
+        _firstStage6Handler = stage6Handlers[0];
+
         Plugin.Log.LogInfo("Patched ui teleport menu");
     }
 
@@ -43,5 +80,10 @@ public static class TeleportMenuPatches
         {
             topHandler.SetActive(true);
         }
+
+        _returnHandler.selectDown = _firstStage1Handler;
+        _returnHandler.selectUp = _firstStage6Handler;
+        _exitHandler.selectDown = _firstStage1Handler;
+        _exitHandler.selectUp = _firstStage6Handler;
     }
 }
