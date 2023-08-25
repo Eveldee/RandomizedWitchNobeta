22eb86049bdcbbfcc268a639d454ed3d43e615d7
Eveldee <eveldee0680@live.fr>
2023-08-25 10:36:34 +0200

    Fix Arcane chant not being disabled

--- Randomizer/RandomizedWitchNobeta/Runtime/Gameplay/ArcaneDisabledPatches.cs
+++ Randomizer/RandomizedWitchNobeta/Runtime/Gameplay/ArcaneDisabledPatches.cs
@@ -22,4 +22,20 @@ public static class ArcaneDisabledPatches
 
         return true;
     }
+
+    [HarmonyPatch(typeof(PlayerInputController), nameof(PlayerInputController.Chant))]
+    [HarmonyPrefix]
+    public static bool InputChantPrefix(PlayerInputController __instance)
+    {
+        var wizardGirl = __instance.controller.wgm;
+
+        if (wizardGirl.GetMagicType() == PlayerEffectPlay.Magic.Null && wizardGirl.GameSave.stats.secretMagicLevel < 1)
+        {
+            Game.AppearEventPrompt("You need at least Arcane level 1 to perform this action.");
+
+            return false;
+        }
+
+        return true;
+    }
 }
