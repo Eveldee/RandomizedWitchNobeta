cf8379c9d760b34af531d63e58776e6768c0572e
Eveldee <eveldee0680@live.fr>
2024-09-20 18:08:45 +0200

    Add game save, seed hash and versions to past runs

--- Randomizer/RandomizedWitchNobeta/Features/UI/RunCompletePatches.cs
+++ Randomizer/RandomizedWitchNobeta/Features/UI/RunCompletePatches.cs
@@ -2,11 +2,16 @@
 using System.IO;
 using HarmonyLib;
 using RandomizedWitchNobeta.Utils;
+using RandomizedWitchNobeta.Utils.Nobeta;
 
 namespace RandomizedWitchNobeta.Features.UI;
 
 public static class RunCompletePatches
 {
+    public static readonly DirectoryInfo NobetaSavesDirectory = new(
+        Path.Combine(Plugin.ConfigDirectory.FullName, "..", "..", "..", "LittleWitchNobeta_Data", "Save")
+    );
+
     [HarmonyPatch(typeof(NPCManage), nameof(NPCManage.Hit))]
     [HarmonyPostfix]
     private static void NpcHitPostfix(NPCManage __instance)
@@ -16,8 +21,9 @@ public static class RunCompletePatches
             return;
         }
 
-        // A boss has been killed, increase global magic level if it's the first time it got killed
-        if (__instance.name is "Boss_Level06" && __instance.GetIsDeath())
+        // Stop timer directly when killing Nonota since we want to let players watch the
+        // credits without being impacted
+        if (__instance.name is NpcUtils.NonotaBossName && __instance.GetIsDeath())
         {
             Plugin.Log.LogDebug("Timer ended!");
             Singletons.Timers.End();
@@ -45,21 +51,24 @@ public static class RunCompletePatches
             beatingGameMessageBox.config.titleText = text;
             beatingGameMessageBox.title.text = text;
 
+            var seedHash = Singletons.RuntimeVariables.Settings.Hash(StartPatches.GameVersionText, StartPatches.RandomizerVersionText);
             Singletons.RuntimeVariables = null;
 
-            ArchiveRun();
+            ArchiveRun(seedHash, StartPatches.GameVersionText, StartPatches.RandomizerVersionText);
         }
     }
 
-    private static void ArchiveRun()
+    private static void ArchiveRun(int seedHash, string gameVersion, string randomizerVersion)
     {
-        var filePath = RuntimeVariables.SavePath;
-        var destinationDirectory = Path.Combine(Plugin.ConfigDirectory.FullName, "PastRuns");
-        var destinationFilePath =
-            Path.Combine(destinationDirectory, $"{DateTime.Now.ToString("s").Replace(':', '.')}.json");
+        string runName = $"{DateTime.Now.ToString("s").Replace(':', '.')} - {seedHash:X8} - {gameVersion[^5..]} - {randomizerVersion[^5..]}";
 
+        var destinationDirectory = Path.Combine(Plugin.ConfigDirectory.FullName, "PastRuns", runName);
         Directory.CreateDirectory(destinationDirectory);
 
-        File.Move(filePath, destinationFilePath);
+        File.Move(RuntimeVariables.SavePath, Path.Combine(destinationDirectory, "RuntimeVariables.json"));
+        File.Move(
+            Path.Combine(NobetaSavesDirectory.FullName, $"GameSave{StartPatches.GameSaveIndex:D2}.dat"),
+            Path.Combine(destinationDirectory, "GameSave.dat"
+        ));
     }
 }
--- Randomizer/RandomizedWitchNobeta/Utils/Nobeta/NpcUtils.cs
+++ Randomizer/RandomizedWitchNobeta/Utils/Nobeta/NpcUtils.cs
@@ -4,6 +4,13 @@ namespace RandomizedWitchNobeta.Utils.Nobeta;
 
 public static class NpcUtils
 {
+    public const string ArmorBossName = "Boss_Act01";
+    public const string TaniaBossName = "Boss_Level02";
+    public const string MonicaBossName = "Boss_Level03_Big";
+    public const string Vanessa1BossName = "Boss_Level04";
+    public const string Vanessa2BossName = "Boss_Level05";
+    public const string NonotaBossName = "Boss_Level06";
+
     public static readonly HashSet<string> ValidBosses =
     [
         "Boss_Act01", // Armor
